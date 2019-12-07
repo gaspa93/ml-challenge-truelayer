56693dede06b81c1f9befaaaeaf0e9430cc1a54f
Mattia Gasparini <mattiagasparini2@gmail.com>
2019-12-07 12:50:53 +0100

    bacis test for movie classifier

--- test.py
+++ test.py
@@ -1,24 +1,54 @@
 import unittest
-from train import MovieClassifierTrainer
+from train import MovieClassifierTrainer, StemmedTfidfVectorizer, stop_words, stemmer
 from movie_classifier import MovieClassifier
 
+
+# test should be run after dataset definition
 class TestTrainModule(unittest.TestCase):
 
     def setUp(self):
         self.movietrainer = MovieClassifierTrainer()
         self.movietrainer.load_data()
 
-    def test_rebalance(self):
+    def test_train_size(self):
         self.movietrainer.split_rebalance()
         self.movietrainer.prepare_features()
-
         self.assertEqual(self.movietrainer.train.shape[0],
                          self.movietrainer.X_train.shape[0],
                          "Train dimension should match")
 
+    def test_test_size(self):
+        self.movietrainer.split_rebalance()
+        self.movietrainer.prepare_features()
         self.assertEqual(self.movietrainer.test.shape[0],
                          self.movietrainer.X_test.shape[0],
                          "Test dimension should match")
+
+
+class TestMovieClassifier(unittest.TestCase):
+
+    def setUp(self):
+        self.mclf = MovieClassifier()
+
+    def test_model_load(self):
+        self.mclf.load_models('models/')
+        self.assertTrue(self.mclf.movieclf, "Model not loaded")
+
+    def test_svd_load(self):
+        self.mclf.load_models('models/')
+        self.assertTrue(self.mclf.svd, "SVD object not loaded")
+
+    def test_tfidf_load(self):
+        self.mclf.load_models('models/')
+        self.assertTrue(self.mclf.tfidf, "TFIDF object not loaded")
+
+    def test_output(self):
+        self.mclf.load_models('models/')
+        result = self.mclf.get_genre('Some Title', 'Some Description')
+
+        self.assertTrue('title' in result.keys(), 'Title missing in output')
+        self.assertTrue('description' in result.keys(), 'Description missing in output')
+        self.assertTrue('genre' in result.keys(), 'Genre missing in output')
 
 if __name__ == '__main__':
     unittest.main()
